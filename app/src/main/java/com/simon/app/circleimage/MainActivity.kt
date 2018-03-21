df61e60689a50d9d75f3b17b7f173a4a7632a86b
xiaowei <SimonX15@163.com>
2018-03-21 17:15:27 +0800

    0.2：demo

--- app/src/main/java/com/simon/app/circleimage/MainActivity.kt
+++ app/src/main/java/com/simon/app/circleimage/MainActivity.kt
@@ -6,12 +6,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    var i = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btn_change.setOnClickListener {
-            circle_iv.setImageResource(R.drawable.ic_other)
+
+            if (i % 2 == 0) {
+                circle_iv.setImageResource(R.drawable.ic_other)
+            } else {
+                circle_iv.setImageResource(R.drawable.ic_time)
+            }
+
+            i++
         }
     }
 }
